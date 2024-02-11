87a5a9911a0c70e23fbca4016e1d625459d92a4e
João Vitor Soares <jvfs12th@gmail.com>
2024-02-11 09:13:47 -0300

    :heavy_plus_sign: build: Cria DevEventDbContext

---
+++ Events.API/Persistence/DevEventDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Events.API.Entities;
+
+namespace Events.API.Persistence
+{
+    public class DevEventDbContext
+    {
+        public List<DevEvent> DevEvents{ get; set; }
+
+        public DevEventDbContext()
+        {
+            DevEvents = new List<DevEvent>();
+        }
+    }
+}
