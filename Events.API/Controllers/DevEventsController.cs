e5f779d3aa8a4b8085fa5e6ab8ec9ef8484293e0
João Vitor Soares <jvfs12th@gmail.com>
2024-02-11 08:56:49 -0300

    :heavy_plus_sign: build: Cria DevEventsController

---
+++ Events.API/Controllers/DevEventsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Events.API.Controllers
+{
+    [Route("api/dev-events")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+    }
+}
