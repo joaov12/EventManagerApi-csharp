ecaf77ad6ea58757cf36eca3e834a75f85d65b71
João Vitor Soares <jvfs12th@gmail.com>
2024-02-11 09:10:53 -0300

    :heavy_plus_sign: build: Cria Entidade DevEventSpeaker

--- Events.API/Entities/DevEventSpeaker.cs
+++ Events.API/Entities/DevEventSpeaker.cs
@@ -2,5 +2,10 @@
 {
     public class DevEventSpeaker
     {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string TalkTitle { get; set; }
+        public string TalkDescription { get; set; }
+        public string LinkedinProfile { get; set; }
     }
 }
