14adbbaee064c944cbcc5f3d1fc33edfa8b23c27
João Vitor Soares <jvfs12th@gmail.com>
2024-02-11 09:02:33 -0300

    :heavy_plus_sign: build: Cria Entidade DevEvent

---
+++ Events.API/Entities/DevEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Events.API.Entities
+{
+    public class DevEvent
+    {
+        public Guid Id { get; set; }  
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<DevEventSpeaker> Speakers { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
