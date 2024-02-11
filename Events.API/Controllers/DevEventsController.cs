3ad2a14ee65881212bc68edece8730b9e4deba9a
João Vitor Soares <jvfs12th@gmail.com>
2024-02-11 09:22:13 -0300

    :sparkles: feat: Adiciona método GetAll no DevEventsController

--- Events.API/Controllers/DevEventsController.cs
+++ Events.API/Controllers/DevEventsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Events.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Events.API.Controllers
 {
@@ -6,5 +7,21 @@ namespace Events.API.Controllers
     [ApiController]
     public class DevEventsController : ControllerBase
     {
+        private readonly DevEventDbContext _context;
+
+        public DevEventsController(DevEventDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var devEvents = _context.DevEvents.Where(d => !d.IsDeleted).ToList();
+
+            return Ok(devEvents);
+        }
+
+
     }
 }
--- Events.API/Entities/DevEventSpeaker.cs
+++ Events.API/Entities/DevEventSpeaker.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Nome { get; set; }
-        public string TalkTitle { get; set; }
+        public string TalkTitle { get; set; }  
         public string TalkDescription { get; set; }
         public string LinkedinProfile { get; set; }
     }
